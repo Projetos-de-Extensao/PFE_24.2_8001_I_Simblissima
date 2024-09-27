34a2a211c369ceefcdbbbbcebc821fc88481f66f
Bernardo Lobo Marques <161499154+bernardolobomarques@users.noreply.github.com>
2024-09-26 21:43:27 -0300

    Add files via upload
    Upando protótipo fiel

---
+++ site_f1_sp/JS/maps.js
@@ -0,0 +1,6 @@
+var map = L.map('mapa-geral').setView([-23.7018, -46.6971], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
