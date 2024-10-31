feebe9e8eaec7908851268dcaf876151cc9282cc
MichelMG-dev <michelmg.dev145@gmail.com>
2024-10-31 16:40:48 -0300

    mudança nav

--- site_simblissima/src/components/Nav/Nav.jsx
+++ site_simblissima/src/components/Nav/Nav.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from './Styles';
+
 function Nav() {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleDropdownClick = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleWindowClick = (e) => {
+    if (!e.target.closest('.dropdown') && !e.target.closest('.dropdown-content')) {
+      setShowDropdown(false);
+    }
+  };
+
   return (
     <Container>
       <nav>
         <img src="/src/media/logo.png" alt="Logo" />
         <div className="dropdown">
-          <button className="dropbtn">Menu</button>
-          <div className="dropdown-content">
+          <button className="dropbtn" onClick={handleDropdownClick}>
+            <img src="/src/media/menu.png" alt="Menu" />
+          </button>
+
+          <div
+            className="dropdown-content"
+            style={showDropdown ? { display: 'block' } : { display: 'none' }}
+          >
             <a href="#programacao">Programação</a>
             <a href="#como-chegar">Como Chegar</a>
             <a href="#ingresso">Ingresso</a>
@@ -15,10 +34,11 @@ function Nav() {
             <a href="#acomodacao">Acomodação</a>
             <a href="#equipes">Equipes</a>
           </div>
-        </div>     
+        </div>
       </nav>
     </Container>
   );
 }
 
 export default Nav;
+
--- site_simblissima/src/components/Nav/Styles.js
+++ site_simblissima/src/components/Nav/Styles.js
@@ -1,66 +1,67 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-          nav {
-  display: flex;
-  align-items: center;
-  background-color: #333;
-  padding: 1em;
-  justify-content: space-between;
-}
+     nav {
+       display: flex;
+       align-items: center;
+       background-color: #333;
+       padding: 1em;
+       justify-content: space-between;
+     }
 
-nav img {
-  width: 100px;
-  height: 100px;
-  margin-right: 20px;
-}
+     nav img {
+       height: 40px;
+       margin-right: 20px;
+     }
 
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
+     .dropdown {
+       position: relative;
+       display: inline-block;
+       height: 20px;
+       width: 20px;
+       padding-right: 10px;
+     }
 
-.dropbtn {
-  background-color: #333;
-  color: #fff;
-  padding: 10px;
-  border: none;
-  cursor: pointer;
-}
+     .dropbtn {
+       background-color: #333;
+       color: #fff;
+       height: 20px;
+       width: 20px;
+       border: none;
+       cursor: pointer;
+     }
 
-.dropbtn:hover {
-  background-color: #444;
-}
+     .dropbtn img {
+       height: 20px;
+       width: 20px;
+       display: flex;
+       justify-content: center;
+       align-items: center;
+     }     
 
+     .dropdown-content {
+       display: none;
+       position: absolute;
+       background-color: #333;
+       min-width: 160px;
+       box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+       right: 0;
+     }
 
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #333;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  right: 0;
-}
+     .dropdown-content a {
+       color: #fff;
+       padding: 12px 16px;
+       text-decoration: none;
+       display: block;
+     }
 
-.dropdown-content a {
-  color: #fff;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
+     .dropdown-content a:hover {
+       background-color: #444;
+     }
 
-.dropdown-content a:hover {
-  background-color: #444;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-.dropdown:hover .dropbtn {
-  background-color: #444;
-}
-
+     .dropdown:hover .dropdown-content {
+       display: block;
+     }
 `;
 
 export { Container }
