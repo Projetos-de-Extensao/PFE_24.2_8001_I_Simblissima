130c613643cec44355b1d4c68f30e3cd8488d3ba
Bernardo Lobo Marques <bernardolobomarques@gmail.com>
2024-11-02 13:30:17 -0300

    "alterações como chegar e ingresso"

--- site_simblissima/src/components/Ingresso/Ingresso.jsx
+++ site_simblissima/src/components/Ingresso/Ingresso.jsx
@@ -12,11 +12,10 @@ function Ingressos() {
 
   return (
     <Container>
-      <div className="container" id="ingresso">
-      <section className="section">
+      <section className="container">
         <h2>Venda de ingressos do evento</h2>
 
-        <h2>Fanzone:</h2>
+        <h3>Fanzone:</h3>
         <p >A Fanzone nos eventos de Fórmula 1 é uma área destinada aos fãs, onde eles podem participar de várias atividades relacionadas ao automobilismo.</p>
         <ul>
           <li>Atividades interativas: Jogos, simuladores de corrida e competições.</li>
@@ -24,8 +23,11 @@ function Ingressos() {
           <li>Loja de produtos: Mercadorias oficiais da F1, como roupas e acessórios.</li>
           <li>Encontros: Oportunidades para conhecer pilotos, equipes ou celebridades do esporte.</li>
         </ul><br />
+        <button class='button' onClick={handleComprarIngressos}>
+          Comprar ingressos da corrida
+        </button>
 
-        <h2>O evento:</h2>
+        <h3>O evento:</h3>
         <p>A corrida de Fórmula 1 é uma competição automobilística de alta velocidade, realizada em circuitos fechados ao redor do mundo. Os eventos
           consistem em uma série de voltas, onde os pilotos competem em carros projetados para desempenho máximo. As corridas geralmente incluem:</p>
         <ul>
@@ -34,19 +36,10 @@ function Ingressos() {
           <li>Estratégias de pit stop: Os pilotos fazem paradas para trocar pneus e reabastecer, influenciando o desempenho.</li>
           <li>Pontos: Os pilotos e equipes acumulam pontos com base na classificação final, que determina os campeões da temporada.</li>
         </ul>
-
-          <div className="button-container">
-            <button class='button' onClick={handleComprarIngressos}>
-              Comprar ingressos da corrida
-            </button>
-
-            <button className='button' onClick={handleFanZone}>
-              Comprar ingressos da Fanzone
-            </button>
-          </div>
+        <button className='button' onClick={handleFanZone}>
+          Comprar ingressos da Fanzone
+        </button>
           </section>
-
-      </div>
     </Container>
   );
 }
--- site_simblissima/src/components/Ingresso/Styles.js
+++ site_simblissima/src/components/Ingresso/Styles.js
@@ -1,77 +1,60 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
 
-  }
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 
-  body {
-  font-family: Arial, sans-serif;
-  line-height: 1.6;
-  background-color: #f5f5f5;
-  color: #333;
+}
 
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px 20px;
+    margin-top: 5px;
+}
 
-  }
+h1, h2, h3 {
+    color: red;
+    margin-bottom: 15px;
+}
 
+p {
+    margin-bottom: 15px;
+    text-align: justify;
+}
 
-  .container {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 10px 20px;
-  margin-top: 5px;
-  }
+ul {
+    list-style-type: none;
+    padding-left: 25px;
+}
 
+ul li {
+    margin: 10px 0;
+}
 
-  h1, h2, h3 {
-  color: #0056b3;
-  margin-bottom: 15px;
-  }
+/* Links */
+a {
+    color: #0056b3;
+    text-decoration: none;
+}
 
-  .section {
-  background: #fff;
-  padding: 15px 20px;
-  margin-top: 0px;
-  border-radius: 8px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  border: 1px solid black;
+a:hover {
+    text-decoration: underline;
+}
 
-  }
+@media (max-width: 768px) {
+    .container {
+        padding: 10px;
+    }
 
-  .button {
-    background-color: #FF0000;
-    color: #FFFFFF;
-    padding: 1rem 2rem;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    margin: 1rem;
-    margin-bottom: 0;
-    font-size: 1.2rem;
-    border: 2px solid #FF0000;
-  }
+    h1, h2, h3 {
+        font-size: 1.5em;
+    }
+}
 
-  .button:hover {
-    background-color: #FFFFFF;
-    color: #FF0000;
-
-  }
-
-  .button-container {
-    padding: 10px;
-    padding-bottom: 0;
-    color: #FF0000;
-    display: flex;
-    itens-align: center;
-    justify-content: center;
-  }
-
-  ul {
-    padding-left: 20px;
-  }
 
 `;
 
