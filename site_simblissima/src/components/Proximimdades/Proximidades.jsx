f163bb35ea4f057ee188fbc23c5ad6568aa79d81
ThiagoNeves123 <143027214+ThiagoNeves123@users.noreply.github.com>
2024-11-13 17:08:03 -0300

    Links para os hoteis no maps

--- site_simblissima/src/components/Proximimdades/Proximidades.jsx
+++ site_simblissima/src/components/Proximimdades/Proximidades.jsx
@@ -19,7 +19,11 @@ const Proximidades = () => {
 
                 <div class="container explicacoes">
                     <h3>Hoteis</h3>
-                    <p>Hotel Coloca o nome: endereço e link pra pagina deles do maps</p>
+                    <p>Hotel GoÁtica:  <a href="https://maps.app.goo.gl/gCrEoFQ5r1hpXnPT6"> Endereço: R. Ática, 590 - Campo Belo, São Paulo - SP, 04634-042</a> </p>
+                    <p>Hotel Blue Tree Premium:  <a href="https://maps.app.goo.gl/7EJpmCXzMD73WgrK7"> Endereço: Av. Roque Petroni Júnior, 1000 - Vila Gertrudes, São Paulo - SP, 04707-000</a> </p>
+                    <p>Hotel Mercure:  <a href="https://maps.app.goo.gl/ms77N43E2dU87ybV7"> Endereço: Rua Professor Manoelito de Ornellas, 104 - Chácara Santo Antônio, São Paulo - SP, 04719-040</a> </p>
+                    <p>Hotel Ibis:  <a href="https://maps.app.goo.gl/Y2CUZr6utGPhdVtx9"> Endereço: Av. Roque Petroni Júnior, 800 - Torre I - Brooklin, São Paulo - SP, 04707-000</a> </p>
+                    <p>Hotel OYO:  <a href="https://maps.app.goo.gl/eNEqULneDoe2m9B16"> Endereço: Av. Santo Amaro, 3484 - Brooklin, São Paulo - SP, 04562-000</a> </p>
 
                     <h3>Delegacias</h3>
                     <p>Delegacia Coloca o nome: endereço e link pra pagina deles do maps</p>
